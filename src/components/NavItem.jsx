e7aa16542d332e0daf3fde4e37702525b41d0b4e
famous-kan <Kanyakorn_fame@hotmail.com>
2024-12-18 02:09:46 +0700

    Fix NavBar

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const NavItem = () => {
+const NavItem = ({label,href ,items}) => {
+    const [isHover, setIsHover] = useState(false)
+
+    
+  if(items){
+    return(
+        <li className='relative'
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        >
+          <a className="flex items-center  text-sm font-medium text-slate-700 no-underline p-2 hover:cursor-pointer">
+           {label}
+          </a>
+        {isHover && (
+            <>
+            <div className="fixed left-0 right-0 mt-0 bg-white z-20">
+               <div className=" mx-auto px-4 py-2">
+               <ul className="grid grid-cols-3 w-2/4 mt-3 mx-56 ">
+                {items.map((item) =>(
+                    <li>
+                        <span className='block text-sm text-gray-900'>
+                        {item.label}
+                        </span>
+
+                        {item.subitems && (
+                            <ul className='space-y-2 m-0 p-0 mt-3 '>
+                                {item.subitems.map((subitem,index) => (
+                                 <li className=''>
+                                <a href={subitem.href}
+                                 className= {`text-black no-underline text-sm ${ item.isLarge ? 'text-xl' : 'text-sm'} `} 
+                                    >{subitem.label}</a>
+                                 </li>   
+                                ))}
+                            </ul>
+                        )}
+
+                    </li>
+
+                ))}
+
+               </ul>
+               </div>
+            </div>
+            </>
+        )}
+        </li>   
+
+    )
+  }  
+
+
   return (
-    <div>NavItem</div>
+    <li>
+      <a
+        href={href || '#'}
+        className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+      >
+        {label}
+      </a>
+    </li>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,53 @@ const Navbar = () => {
     
   return (
     
-    <div className=' fixed top-0 flex justify-between shadow-lg items-center h-12 w-full bg-white/75 backdrop-blur-sm z-50'>
+    <div className=' fixed top-0 flex shadow-lg h-12 w-full bg-white/75 backdrop-blur-sm z-50'>
         
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 justify-center mx-auto items-center m-0 p-0"> 
 
-          <div className=''>
+          <Link to="/">
           <svg height="44" viewBox="0 0 14 44" width="14" xmlns="http://www.w3.org/2000/svg"><path d="m13.0729 17.6825a3.61 3.61 0 0 0 -1.7248 3.0365 3.5132 3.5132 0 0 0 2.1379 3.2223 8.394 8.394 0 0 1 -1.0948 2.2618c-.6816.9812-1.3943 1.9623-2.4787 1.9623s-1.3633-.63-2.613-.63c-1.2187 0-1.6525.6507-2.644.6507s-1.6834-.9089-2.4787-2.0243a9.7842 9.7842 0 0 1 -1.6628-5.2776c0-3.0984 2.014-4.7405 3.9969-4.7405 1.0535 0 1.9314.6919 2.5924.6919.63 0 1.6112-.7333 2.8092-.7333a3.7579 3.7579 0 0 1 3.1604 1.5802zm-3.7284-2.8918a3.5615 3.5615 0 0 0 .8469-2.22 1.5353 1.5353 0 0 0 -.031-.32 3.5686 3.5686 0 0 0 -2.3445 1.2084 3.4629 3.4629 0 0 0 -.8779 2.1585 1.419 1.419 0 0 0 .031.2892 1.19 1.19 0 0 0 .2169.0207 3.0935 3.0935 0 0 0 2.1586-1.1368z"></path></svg>
-          </div>  
+          </Link>  
 
           <NavItem
           label="Store"
           items={[
             { 
-              label: 'Electronics', 
-              href: '/products/electronics',
+              label: 'Shop', 
+              isLarge: true,
               subitems: [
-                { label: 'Smartphones', href: '/products/electronics/smartphones' },
-                { label: 'Laptops', href: '/products/electronics/laptops' },
-                { label: 'More', href: '/products/electronics', isMore: true }
+                { label: 'Shop the Latest', href: "/" },
+                { label: 'Mac', href: '/' },
+                { label: 'iPad', href: '/' },
+                { label: 'iPhone', href: '/' },
+                { label: 'Apple Watch', href: '/' },
+                { label: 'Apple Vision Pro', href: '/' },
+                { label: 'Accesories', href: '/' }
               ]
             },
             { 
-              label: 'Clothing', 
-              href: '/products/clothing',
+              label: 'Quick Links', 
+              isLarge: false,
               subitems: [
-                { label: 'Men\'s Wear', href: '/products/clothing/mens' },
-                { label: 'Women\'s Wear', href: '/products/clothing/womens' },
-                { label: 'More', href: '/products/clothing', isMore: true }
+                { label: 'Shop Holiday Gifts', href: '/' },
+                { label: 'Find a Store', href: '/products/clothing/womens' },
+                { label: 'Order Status ', href: '/' },
+                { label: 'Apple Trade In ', href: '/' },
+                { label: 'Financing', href: '/' },
               ]
             },
-            { label: 'Books', href: '/products/books' },
+            { 
+              label: 'Shop Special Stores', 
+              isLarge: false,
+              subitems: [
+                { label: 'Certified Refurbished', href: '/' },
+                { label: 'Education', href: '/products/clothing/womens' },
+                { label: 'Business', href: '/' },
+                { label: 'Veterans and Military', href: '/' },
+                { label: 'Governemt', href: '/' },
+              ]
+            },
+
           ]}
         />
           
